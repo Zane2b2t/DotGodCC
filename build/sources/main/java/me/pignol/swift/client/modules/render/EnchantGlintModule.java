6a899f1b8ef5848f7adc238f2bbbbde1b4f98074
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-03-26 14:55:12 +0200

    Update EnchantGlintModule.java

--- build/sources/main/java/me/pignol/swift/client/modules/render/EnchantGlintModule.java
+++ build/sources/main/java/me/pignol/swift/client/modules/render/EnchantGlintModule.java
@@ -11,6 +11,8 @@ public class  EnchantGlintModule extends Module {
     public final Value<Integer> red = new Value<>("Red", 255, 0, 255);
     public final Value<Integer> green = new Value<>("Green", 255, 0, 255);
     public final Value<Integer> blue = new Value<>("Blue", 255, 0, 255);
+    public static final Value<Float> glintScale = new Value<>("GlintSize", 0.1f, 5.0f, 25.0f);
+    public static final Value<Float> glintSpeed = new Value<>("GlintSpeed", 0.1f, 1.0f, 15.0f);
     public final Value<Boolean> rainbow = new Value<>("Rainbow", false);
 
     public EnchantGlintModule() {
