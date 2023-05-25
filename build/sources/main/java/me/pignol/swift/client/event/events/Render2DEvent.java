af368a0d03efefd3247b8d16243e6e18c04bd612
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-05-25 12:50:39 +0300

    Delete build directory

--- build/sources/main/java/me/pignol/swift/client/event/events/Render2DEvent.java
+++
@@ -1,6 +0,0 @@
-package me.pignol.swift.client.event.events;
-
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-public class Render2DEvent extends Event {
-}
--- build/sources/main/java/me/pignol/swift/client/event/events/Render3DEvent.java
+++
@@ -1,6 +0,0 @@
-package me.pignol.swift.client.event.events;
-
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-public class Render3DEvent extends Event {
-}
