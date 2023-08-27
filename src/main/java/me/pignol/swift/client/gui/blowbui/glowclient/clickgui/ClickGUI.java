358089192c243034e4b4c8ce1146853d0a9c6dd6
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-08-27 13:48:57 +0300

    Update ClickGUI.java

--- src/main/java/me/pignol/swift/client/gui/blowbui/glowclient/clickgui/ClickGUI.java
+++ src/main/java/me/pignol/swift/client/gui/blowbui/glowclient/clickgui/ClickGUI.java
@@ -36,6 +36,7 @@ public class ClickGUI extends GuiScreen {
 
    public void drawScreen(int x, int y, float ticks) {
       windows.forEach(window -> window.draw(x, y));
+      this.drawDefaultBackground();
    } {
     //  this.drawDefaultBackground();
    }
